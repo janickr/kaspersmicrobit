bc97f8e13ec565c3882690964ddad84efa43edf0
Janick Reynders <janick.reynders@gmail.com>
2022-02-11 00:05:32 +0100

    enable autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ release = '0.0.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.napoleon'
 ]
 napoleon_google_docstring = True
