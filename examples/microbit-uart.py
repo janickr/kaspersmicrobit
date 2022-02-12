4dda0b114e6beaf718af1fb15de1ed06926b092c
Janick Reynders <janick.reynders@gmail.com>
2022-02-12 16:55:52 +0100

    uart example, moved other examples

---
+++ examples/microbit-uart.py
@@ -0,0 +1,23 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import time
+
+from kaspersmicrobit import KaspersMicrobit
+
+MICROBIT_BLUETOOTH_ADDRESS = 'E3:7E:99:0D:C1:BA'
+
+
+def print_received_string(string: str):
+    print(f"Received from microbit: '{string}'")
+
+
+with KaspersMicrobit(MICROBIT_BLUETOOTH_ADDRESS) as microbit:
+    # You can listen for strings sent by the microbit
+    microbit.uart.receive_string(print_received_string)
+
+    # You can send a string to the microbit
+    microbit.uart.send_string("Hello kasper, this is working out very well\n")
+
+    time.sleep(25)
