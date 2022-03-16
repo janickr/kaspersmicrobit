74931b108e505a035ebae290af417e04be3b315c
Janick Reynders <janick.reynders@gmail.com>
2022-03-16 21:35:42 +0100

    update copyright notice

--- tests/test_io_pin.py
+++ tests/test_io_pin.py
@@ -1,3 +1,7 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from kaspersmicrobit.services.io_pin import Pin, PinValue, PwmControlData, PinIO, PinAD, PinConfiguration, \
     PinIOConfiguration, PinADConfiguration
 
