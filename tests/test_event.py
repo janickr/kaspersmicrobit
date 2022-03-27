01cc456424a9d55dbcb2a13daac6511912a4badb
Janick Reynders <janick.reynders@gmail.com>
2022-03-28 01:03:54 +0200

    fixed tests (device ids changed)

--- tests/test_event.py
+++ tests/test_event.py
@@ -9,22 +9,22 @@ import kaspersmicrobit.services.v1_events as v1_events
 class TestEvent:
     def test_to_bytes(self):
         event = Event(v1_events.DEVICE_ID_GESTURE, v1_events.EVENT_GESTURE_3G)
-        assert event.to_bytes() == bytearray.fromhex("1b 00 08 00")
+        assert event.to_bytes() == bytearray.fromhex("0d 00 08 00")
 
     def test_from_bytes(self):
         event = Event(v1_events.DEVICE_ID_GESTURE, v1_events.EVENT_GESTURE_3G)
-        assert Event.from_bytes(bytearray.fromhex("1b 00 08 00")) == event
+        assert Event.from_bytes(bytearray.fromhex("0d 00 08 00")) == event
 
     def test_list_to_bytes(self):
         events = [
             Event(v1_events.DEVICE_ID_GESTURE, v1_events.EVENT_GESTURE_3G),
             Event(v1_events.DEVICE_ID_DISPLAY, v1_events.EVENT_DISPLAY_ANIMATION_COMPLETE)
         ]
-        assert Event.list_to_bytes(events) == bytearray.fromhex("1b 00 08 00 06 00 01 00")
+        assert Event.list_to_bytes(events) == bytearray.fromhex("0d 00 08 00 07 00 01 00")
 
     def test_list_from_bytes(self):
         events = [
             Event(v1_events.DEVICE_ID_GESTURE, v1_events.EVENT_GESTURE_3G),
             Event(v1_events.DEVICE_ID_DISPLAY, v1_events.EVENT_DISPLAY_ANIMATION_COMPLETE)
         ]
-        assert Event.list_from_bytes(bytearray.fromhex("1b 00 08 00 06 00 01 00")) == events
+        assert Event.list_from_bytes(bytearray.fromhex("0d 00 08 00 07 00 01 00")) == events
